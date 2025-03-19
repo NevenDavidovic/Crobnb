651dd793fa754c54389a4b01cde7e2a395ac5b19
NevenDavidovic <nenoronnie@gmail.com>
2025-03-20 00:00:10 +0100

    return to default

--- utils/directus/client.ts
+++ utils/directus/client.ts
@@ -1,4 +1,3 @@
-// client.ts
 import { createDirectus, rest, authentication } from "@directus/sdk";
 import type { Schema } from "~/types/directus/exports/schema";
 import { useCookie } from "#app";
@@ -11,13 +10,7 @@ export const createDirectusClient = (directusUrl: string) => {
   const accessToken = useCookie("directus_access_token");
 
   if (accessToken.value) {
-    const route = useRoute();
-    const publicRoutes = ["/home", "/", "/novosti", "/regije", "/smjestaj"];
-    const isPublicRoute = publicRoutes.some((r) => route.path.startsWith(r));
-
-    if (!isPublicRoute) {
-      client.setToken(accessToken.value as string);
-    }
+    client.setToken(accessToken.value as string);
   }
 
   return client;
