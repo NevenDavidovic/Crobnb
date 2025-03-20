fc8be15c1a21d100ce29f9f89aa546a9da190f0e
NevenDavidovic <nenoronnie@gmail.com>
2025-03-20 12:08:59 +0100

    removing comments

--- plugins/directus-error-handler.ts
+++ plugins/directus-error-handler.ts
@@ -1,4 +1,3 @@
-// plugins/directus-error-handler.ts
 import { defineNuxtPlugin } from "#app";
 
 export default defineNuxtPlugin(() => {
@@ -6,21 +5,14 @@ export default defineNuxtPlugin(() => {
     const accessToken = useCookie("directus_access_token");
     const refreshToken = useCookie("directus_refresh_token");
 
-    // Global error handler for Directus API errors
     window.addEventListener("unhandledrejection", (event) => {
       const error = event.reason;
 
-      // Check for Directus token expiration
       if (error?.errors?.[0]?.extensions?.code === "TOKEN_EXPIRED") {
-        // Don't show the error in console
         event.preventDefault();
 
-        // Clear tokens
         accessToken.value = null;
         refreshToken.value = null;
-
-        // Optionally refresh the page to reset the app state
-        // window.location.reload();
 
         console.log("Cleared expired tokens");
       }
